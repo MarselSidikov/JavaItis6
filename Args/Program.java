30cdfe5daadb81d9bbcf7fd0551b33bfe9c06dfc
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-06-07 13:17:14 +0300

    add comments

--- Args/Program.java
+++ Args/Program.java
@@ -1,11 +1,17 @@
 class Program {
 	public static void main(String[] args) {
+		// Сообщение для вывода
 		String message = "Hello ,";
+		// переменная цикла, необходима для 
+		// учета количества аргументов, поданных на вход
 		int i = 0;
+		// пока i меньше, чем количество аргументов -1
 		while (i < args.length - 1) {
+			// прикрепляем следующее имя через запятую
 			message = message + " " + args[i] + ",";
 			i = i + 1;
 		}
+		// последнее имя прикрепляем через and
 		message = message + " and " + args[i];
 		System.out.println(message);
 	}
