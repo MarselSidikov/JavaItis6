b18e15db6510407aec069debcbbb0fe8c8725c2c
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-06-08 16:03:13 +0300

    add comments to number

--- Numbers/Program.java
+++ Numbers/Program.java
@@ -1,22 +1,37 @@
+// включаем библиотеку для работы со случайными числами
 import java.util.Random;
+// включаем библиотеку для считывания с клавиатуры
 import java.util.Scanner;
+// основной код программы
 class Program {
+	// main - входная точка приложения. 
+	// приложение начинает запуск с этой функции
 	public static void main(String args[]) {
+		// завели переменную, целое число
+		// здесь мы будем хранить загаданное компьютером число
 		int a;
-		
+		// создаем генератор случайных чисел
 		Random random = new Random();
+		// генерируем случайное число
 		a = random.nextInt(10);
-
+		// создаем считыватель с клавиатуры
 		Scanner scanner = new Scanner(System.in);
+		// считываем (вводим) пользовательское число с клавиатуры
 		int x = scanner.nextInt();
-		if (x < a) {
-			System.out.println("Bolshe");
-			System.out.println(a);
-		} else if (x == a) {
-			System.out.println("URA!!!!");
-		} else {
-			System.out.println("Menshe");
-			System.out.println(a);
+		// пока число, которое ввел пользователь не совпало
+		// с загаданным компьютером числом
+		while (x != a) {
+			// говорим, меньше оно или больше
+			if (x < a) {
+				System.out.println("Bolshe");
+			} else if (x > a) {
+				System.out.println("Menshe");
+			}
+			// считываем новое число пользовтеля
+			x = scanner.nextInt();
 		}
+		// если пользователь угадал, то мы выходим из цикла
+		// и попадаем сюда
+		System.out.println("URA!");
 	}
 }
