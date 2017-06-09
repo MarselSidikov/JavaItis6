bba282a11439870a4239ad929dc847c0bc767809
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-06-09 10:59:55 +0300

    add char

--- Chars/Program.java
+++ Chars/Program.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 class Program {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		char c = 'A';
 		c = (char)(c + 1);
 		System.out.println(c);
@@ -7,5 +9,9 @@ class Program {
 		System.out.println(code);
 		c = (char)67;
 		System.out.println(c);
+		c = scanner.next().charAt(0);
+		System.out.println(c);
+		code = c;
+		System.out.println(code);
 	}
 }
