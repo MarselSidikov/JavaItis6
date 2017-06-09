26e33041fc423b8b836095d82262fee5834f55fd
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-06-09 10:54:37 +0300

    add char

---
+++ Chars/Program.java
@@ -0,0 +1,11 @@
+class Program {
+	public static void main(String[] args) {
+		char c = 'A';
+		c = (char)(c + 1);
+		System.out.println(c);
+		int code = c;
+		System.out.println(code);
+		c = (char)67;
+		System.out.println(c);
+	}
+}
