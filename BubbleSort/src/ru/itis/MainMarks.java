96b855d8c1e94565854f52a2b9bcdb07742c9ccb
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-06-15 11:59:59 +0300

    add sorts and procedures

--- BubbleSort/src/ru/itis/MainMarks.java
+++ BubbleSort/src/ru/itis/MainMarks.java
@@ -1,6 +1,45 @@
 package ru.itis;
 
 public class MainMarks {
+    /**
+     * Подпрограмма-процедура, сортирующая массив пузырьком
+     * @param a - сортируемый массив, формальный параметр, сюда будет передан конкретный массив
+     */
+    public static void bubbleSort(int a[]) {
+        for (int i = a.length -1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (a[j] > a[j+1]) {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void insertionSort(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            int min = a[i];
+            int minIndex = i;
+            for (int j = i; j < a.length; j++) {
+                if (a[j] < min) {
+                    min = a[j];
+                    minIndex = j;
+                }
+            }
+            a[minIndex] = a[i];
+            a[i] = min;
+        }
+    }
+
+    public static void showMatrix(int a[][]) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         // оценки студента
         int marks[][] = {
@@ -8,9 +47,14 @@ public class MainMarks {
                 {3, 2, 2, 1, 5},
                 {1, 2, 2, 5, 4},
                 {4, 4, 3, 5, 5},
-                {3, 3, 3, 3, 3},
+                {3, 4, 5, 4, 5},
                 {1, 5, 1, 4, 3}};
-        // ЗАДАЧА - отсортировать каждую строчку массива и
-        // и вывести все на экран
+
+        for (int i = 0; i < marks.length; i++) {
+            // вызов процедуры, marks[i] - аргумент
+            insertionSort(marks[i]);
+        }
+        // marks - аргумент
+        showMatrix(marks);
     }
 }
