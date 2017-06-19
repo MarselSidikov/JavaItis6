a73c02be17d751776030c6e3cb9c8785107cdc23
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-06-19 11:10:16 +0300

    add oop

---
+++ OOP/src/ru/itis/Main.java
@@ -0,0 +1,24 @@
+package ru.itis;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    	Student marsel = new Student();
+	    marsel.age = 23;
+	    marsel.name = "Marsel";
+	    marsel.height = 185;
+
+	    Student sergey = new Student();
+	    sergey.name = "Sergey";
+	    sergey.age = 16;
+	    sergey.height = 180;
+
+	    // int a[] = {4, 5, 6};
+	    Student students [] = {marsel, sergey};
+
+
+
+
+    }
+}
---
+++ OOP/src/ru/itis/Student.java
@@ -0,0 +1,7 @@
+package ru.itis;
+
+public class Student {
+    String name;
+    int age;
+    double height;
+}
