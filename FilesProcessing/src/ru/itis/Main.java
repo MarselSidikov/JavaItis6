3c994126a3a0a8d85bdf369c6e6c827aa3ab7c3c
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-06-29 12:38:24 +0300

    add parser

--- FilesProcessing/src/ru/itis/Main.java
+++ FilesProcessing/src/ru/itis/Main.java
@@ -1,19 +1,16 @@
 package ru.itis;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        WordFinder finder = new WordFinder();
-
-        Scanner scanner = new Scanner(System.in);
-        String word = scanner.next();
-
-        long startTime = System.currentTimeMillis();
-        finder.findCountOfWordInFolder(word, "files");
-        long stopTime = System.currentTimeMillis();
-        long elapsedTime = stopTime - startTime;
-        System.out.println(elapsedTime);
+       WordFinder finder = new WordFinder("hello", "files");
+       ArrayList<String> dictionary = finder.createDictionary();
+       for (String word : dictionary) {
+           System.out.println(word);
+       }
     }
 }
